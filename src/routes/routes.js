1d518a91c0cae3d95e89b1cb82c2aad9ba30720b
Eduardo Nakao <eduh_erick@hotmail.com>
2023-06-05 22:21:14 -0300

    adicionado model e controle de produtos

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,17 @@
 const router = require('express').Router()
+const ProductController = require('../controllers/products')
 
-router.get('/clientes', (req, res) => {
 
-    res.send({
-        ok: 123
-    })
-})
+// VERBOS HTTP (4 TIPOS)
+// GET - obter dados
+// POST - enviar/receber dados
+// PUT - atualizar dados
+// DELETE - remover dados
+
+
+router.get('/products', ProductController.get)
+//router.post('/products', ProductController.post)
+//router.put('/products/:id', ProductController.put)
+//router.delete('/products/:id', ProductController.delete)
 
 module.exports = router
